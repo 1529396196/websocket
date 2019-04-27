2f2ef0157b8ca5fc1891bb3f7baca26cfb4d3d15
brag <1529396196@example.com>
2019-04-27 21:30:33 +0800

    源码
    要开启socket扩展才可以用

---
+++ download.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/10
+ * Time: 22:25
+ */
+for ($i=1;$i<=79;$i++){
+//    $r = file_get_contents('http://www.yxsss.com/ui/sk/'.$i.'.gif');
+//    $a = file_put_contents('./sk/'.$i.'.gif',$r);
+}
+
+//var_dump($r);
+var_dump($i);
